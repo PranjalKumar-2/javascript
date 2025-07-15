a9f449bc47e228926befe78389d13b1017ae5160
PranjalKumar-2 <120299727+PranjalKumar-2@users.noreply.github.com>
2025-07-15 07:38:20 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,11 @@
+const pi = 3.14 
+// pi = 22/7 can not change the const 
+let accountEmail = "pranjal28@s.com"
+var password = "8324-021"
+acccontcity = "dehradun"
+
+accountEmail= "hero@s.com"
+password ="asdfas"
+acccontcity = "dehli"
+let accountState;
+console.table([accountEmail , pi , password , acccontcity ,accountState])
