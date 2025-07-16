93f024261fc25c3d346d74b224b6345863e98cf0
PranjalKumar-2 <120299727+PranjalKumar-2@users.noreply.github.com>
2025-07-16 14:58:21 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+//let score = "Pranjal"
+//let score = "33asdf" //NAN
+//let score = undefined //NAN
+//let score = true // 1 false=0
+
+let score = null //0
+
+
+console.log(typeof score);
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
+  
+//let isLoggedIn = ""// false
+//let isLoggedIn = "pranjal"// true
+let isLoggedIn = ""// true
+let booleanISloggedIn =Boolean(isLoggedIn)
+console.log(booleanISloggedIn);
+
