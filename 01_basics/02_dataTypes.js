694028e9a1e053e7ccc91b8fb77ee5b0419894d5
PranjalKumar-2 <120299727+PranjalKumar-2@users.noreply.github.com>
2025-07-16 14:44:22 +0000

    dataType

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict";// treat all js code as newer version 
+
+//alert(30 +3) //we are using nodejs, not browser 
+
+console.log( 3 + 3 )
+
+let name ="Pranjal "
+let age =23
+let isLoggedIn = false 
+let state ;
+
+/*
+number => 2 to power 52 
+bigint 
+string => ""
+boolean => true/false
+null => standalone value 
+undefined => 
+symbol => unique 
+
+
+object 
+
+*/
+
+console.log(typeof null )
+console.log(typeof  undefined )
