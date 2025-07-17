bde6ebfd976fc48ca7fd1ec62b336fe566e81eab
PranjalKumar-2 <120299727+PranjalKumar-2@users.noreply.github.com>
2025-07-17 11:16:02 +0000

    today revision

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -6,17 +6,52 @@
 let score = null //0
 
 
-console.log(typeof score);
-console.log(typeof(score))
+// console.log(typeof score);
+// console.log(typeof(score))
 
 let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber)
-console.log(valueInNumber);
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
   
 //let isLoggedIn = ""// false
 //let isLoggedIn = "pranjal"// true
 let isLoggedIn = ""// true
 let booleanISloggedIn =Boolean(isLoggedIn)
-console.log(booleanISloggedIn);
+// console.log(booleanISloggedIn);
 
+/*--------Operation--------------*/
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Pranjal"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
