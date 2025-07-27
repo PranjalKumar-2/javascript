29e316b99f8d22b57b5721da436bd368d6046451
PranjalKumar-2 <120299727+PranjalKumar-2@users.noreply.github.com>
2025-07-27 04:40:34 +0000

    fuction-pera

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -39,3 +39,33 @@ function loginUserMessage(username = "sam"){
 
 // console.log(loginUserMessage("PRANJAL"))
 // console.log(loginUserMessage("PRANJAL"))
+
+function calculateCartPrice(...num1){
+     return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500 ));
+ 
+const user = {
+    username : "Pranjal",
+    price: 101
+}
+
+function handleObject(anyObject){
+   console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+
+}
+
+// handleObject(user)
+handleObject({
+    username : "don",
+    price: 399
+})
+
+const MyNewArray = [1213 , 123, 234,345]
+function returnSecondValue(getArray){
+    return getArray[1]
+
+}
+console.log(returnSecondValue(MyNewArray));
+console.log(returnSecondValue([3124, 1234, 435,56543]));
